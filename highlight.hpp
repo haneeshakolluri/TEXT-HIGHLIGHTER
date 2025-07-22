b8cdc8231e240412d055150942a75a10539060f8
Haneesha Kolluri <151764166+haneeshakolluri@users.noreply.github.com>
2025-07-22 16:08:36 +0530

    Create highlight.hpp

---
+++ highlight.hpp
@@ -0,0 +1,12 @@
+highlight.hpp 
+  
+#ifndef HIGHLIGHT_HPP 
+#define HIGHLIGHT_HPP 
+#include <iostream> 
+4 
+5  
+#include <string> 
+ 
+void highlight_token(const std::string& token_type, const std::string& lexeme); 
+ 
+#endif 
